d33eab25f5137faf1e7dd8365863ce5a39699661
Cache Staheli <cache@byu.edu>
2016-06-03 17:51:35 -0600

    initial classes added

---
+++ src/sphero_swarm_line_demo/src/agents.py
@@ -0,0 +1,32 @@
+from game import *
+
+
+class Agent:
+    def __init__(self, index, isPacman=False):
+        self.index = index
+        self.isPacman = isPacman
+        self.location = Location(0, 0)
+
+    def takeAction(self, gameState):
+        return
+
+
+class PacmanAgent(Agent):
+    def __init__(self, index):
+        Agent.__init__(self, index)
+
+    def takeAction(self, gameState):
+        return Directions.NORTH
+
+
+class GhostAgent(Agent):
+    def __init__(self, index):
+        Agent.__init__(self, index)
+
+    def takeAction(self, gameState):
+        return Directions.SOUTH
+
+
+class AgentState:
+    def __init__(self, location):
+        self.location = location
---
+++ src/sphero_swarm_line_demo/src/game.py
@@ -0,0 +1,64 @@
+class Directions:
+    NORTH = 'North'
+    SOUTH = 'South'
+    EAST = 'East'
+    WEST = 'West'
+    STOP = 'Stop'
+
+    LEFT = {NORTH: WEST,
+            SOUTH: EAST,
+            EAST: NORTH,
+            WEST: SOUTH,
+            STOP: STOP}
+
+    RIGHT = dict([(y, x) for x, y in LEFT.items()])
+
+    REVERSE = {NORTH: SOUTH,
+               SOUTH: NORTH,
+               EAST: WEST,
+               WEST: EAST,
+               STOP: STOP}
+
+
+class Location:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return "(%s,%s)" % (self.x, self.y)
+
+
+class GameBoard:
+    def __init__(self):
+        self._board = {}
+
+    def initializeBoard(self):
+        return
+
+
+class GameState:
+    def __init__(self, agents, gameBoard):
+        self.agents = agents
+        self.gameBoard = gameBoard
+
+    def containsPacman(self, location):
+        for agent in self.agents:
+            if agent.isPacman:
+                if agent.location == location:
+                    return True
+        return False
+
+    def containsGhost(self, location):
+
+        for agent in self.agents:
+            if not agent.isPacman:
+                if agent.location == location:
+                    return True
+        return False
